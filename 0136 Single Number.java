f49574920a7e4104a31e0f47fc6c6726140e4d0a
sanjibbaral435 <sanjibbaral.nit@gmail.com>
2018-12-20 17:05:25 -0600

    Added new leetcode solutions

---
+++ 0136 Single Number.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        if(nums.length == 0) return 0;
+        int res = 0;
+        for(int i=0;i<nums.length;i++){
+            res = res^nums[i];
+        }
+        return res;
+    }
+}
