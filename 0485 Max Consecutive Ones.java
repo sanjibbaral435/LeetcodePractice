dde3b2ffca338b416ef3d83f04695fd4204d80e4
sanjibbaral435 <sanjibbaral.nit@gmail.com>
2018-12-28 00:01:54 -0600

    Added new leetcode solution

---
+++ 0485 Max Consecutive Ones.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        
+        int max = -1;
+        int count = 0;
+        for (int num : nums) {
+            if (num == 1) {
+                count++;
+            } else {
+                max = Math.max(max, count);
+                count = 0;
+            }
+        }
+        
+        return Math.max(max, count);
+    }
+}
