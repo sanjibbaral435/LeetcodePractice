e11d713e71ea447a5fcb4626c901ecb194df9c67
sanjibbaral435 <sanjibbaral.nit@gmail.com>
2019-01-06 13:43:40 -0700

    Added new leetcode solution

---
+++ 0704 Binary Search.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        if(nums.length == 0) return -1;
+        int lo = 0;
+        int hi = nums.length-1;
+        while(lo <= hi){
+            int mid = (lo+hi)/2;
+            if(target < nums[mid]){
+                hi = mid-1;
+            } else if(target > nums[mid]){
+                lo = mid+1;
+            } else return mid;
+        }
+        return -1;
+    }
+}
