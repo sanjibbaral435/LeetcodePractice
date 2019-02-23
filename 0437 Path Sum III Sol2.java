2c962c19d9ce485e4b166c6f3514f4ec6e255b77
sanjibbaral435 <sanjibbaral.nit@gmail.com>
2019-02-22 22:27:32 -0600

    Added new solutions

---
+++ 0437 Path Sum III Sol2.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+// The Prefix Sum Method.
+// At each recursion function, we first check how many paths with currSumm - target exist. then we add that number of paths + left Tree and Right tree
+
+// then we increase the currSumm path count by 1
+// from every recursion the res, we return, is basically the number of paths with that sum, where the path ends at this rrot (path starting from any parent node)
+// Complexity O(n)
+
+
+class Solution {
+    public int pathSum(TreeNode root, int sum) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);  //Default sum = 0 has one count
+        return backtrack(root, 0, sum, map); 
+    }
+    //BackTrack one pass
+    public int backtrack(TreeNode root, int sum, int target, Map<Integer, Integer> map){
+        if(root == null)
+            return 0;
+        sum += root.val;
+        int res = map.getOrDefault(sum - target, 0);    //See if there is a subarray sum equals to target
+        map.put(sum, map.getOrDefault(sum, 0)+1);
+        //Extend to left and right child
+        res += backtrack(root.left, sum, target, map) + backtrack(root.right, sum, target, map);
+        map.put(sum, map.get(sum)-1);   //Remove the current node so it wont affect other path
+        return res;
+    }
+}
