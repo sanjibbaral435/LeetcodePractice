a2263eb0dfd7da5d3ef8a374bf36aad868d48f5a
sanjibbaral435 <sanjibbaral.nit@gmail.com>
2019-03-02 19:19:04 -0600

    Added new Solutions

---
+++ 0402 Remove K Digits.java
@@ -0,0 +1,40 @@
+class Solution {
+    public String removeKdigits(String num, int k) {
+        if(k <= 0) return num;
+        if(k == num.length()) return "0";
+        
+        int i = 0;
+        
+        Stack<Character> stack = new Stack<>();
+        
+        
+        while(i < num.length()){
+            //whenever meet a digit which is less than the previous digit, discard the previous one
+            while(k > 0 && !stack.isEmpty() && stack.peek() > num.charAt(i)){
+                stack.pop();
+                k--;
+            }
+            stack.push(num.charAt(i));
+            i++;
+        }
+        
+        // corner case like "1111"
+        while(k > 0){
+            stack.pop();
+            k--;
+        }
+        
+        //construct the number from the stack
+        
+        StringBuilder sb = new StringBuilder();
+        while(!stack.isEmpty())
+            sb.append(stack.pop());
+        
+        sb.reverse();
+        
+        //remove all the 0 at the head
+        while(sb.length()>1 && sb.charAt(0)=='0')
+            sb.deleteCharAt(0);
+        return sb.toString();
+    }
+}
